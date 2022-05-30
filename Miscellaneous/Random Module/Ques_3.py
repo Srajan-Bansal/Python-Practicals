2e90f16df199108949aab7c8f8ad7a5b53f711ab
Srajan Bansal <87896128+Srajan-Bansal@users.noreply.github.com>
2022-05-30 20:08:17 +0530

    Add files via upload

---
+++ Miscellaneous/Random Module/Ques_3.py
@@ -0,0 +1,14 @@
+# Write a Python program to shuffle the elements of a given list.
+importrandom
+nums=[1,2,3,4,5]
+print("Originallist:")
+print(nums)
+random.shuffle(nums)
+print("Shufflelist:")
+print(nums)
+words=['red','black','green','blue']
+print("\nOriginallist:")
+print(words)
+random.shuffle(words)
+print("Shufflelist:")
+print(words)
