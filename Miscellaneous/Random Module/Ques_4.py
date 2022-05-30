14c5c8c904fa5c55502e60b9fc37f7397c8eaf33
Srajan Bansal <87896128+Srajan-Bansal@users.noreply.github.com>
2022-05-30 20:08:30 +0530

    Add files via upload

---
+++ Miscellaneous/Random Module/Ques_4.py
@@ -0,0 +1,7 @@
+# Write a Python program to generate a float between 0 and 1, inclusive and generate a random float with in a specific range.
+importrandom
+print("Generateafloatbetween0and1,inclusive:")
+print(random.uniform(0,1))
+print("\nGeneratearandomfloatwithinarange:")
+random_float=random.uniform(1.0,3.0)
+print(random_float)
